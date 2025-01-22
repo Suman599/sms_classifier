b5d27d14a3378baca799290856eeab316bb815f8
Your Name <chakrabortysuman599@gmail.com>
2025-01-22 23:24:34 +0530

    all good

--- app.py
+++ app.py
@@ -3,47 +3,48 @@ import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
+
+# Ensure punkt and stopwords are downloaded
 nltk.download('punkt')
 nltk.download('stopwords')
-from nltk.stem.porter import PorterStemmer
-ps=PorterStemmer()
 
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
 
 
 def transform_text(text):
     text = text.lower()
-    text = nltk.word_tokenize(text)
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
-    text = y[:]
-    y.clear()
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
+    tokens = nltk.word_tokenize(text)
+    filtered_tokens = [
+        ps.stem(token) for token in tokens
+        if token.isalnum() and token not in stopwords.words('english') and token not in string.punctuation
+    ]
+    return " ".join(filtered_tokens)
 
-    text = y[:]
-    y.clear()
-    for i in text:
-        y.append(ps.stem(i))
 
-    return " ".join(y)
+# Load the vectorizer and model
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
-tfidf=pickle.load(open('vectorizer.pkl','rb'))
-model=pickle.load(open('model.pkl','rb'))
 st.title("Email/SMS Spam Classifier")
-input_sms=st.text_input("Enter the message")
-if st.button('Predict'):
+input_sms = st.text_input("Enter the message")
 
-    #1.preprocess
-    transformed_sms=transform_text(input_sms)
-    #2.vectorize
-    vector_input=tfidf.transform([transformed_sms])
-    #3.predict
-    result=model.predict(vector_input)[0]
-    #4.display
-    if result==1:
-        st.header("Spam")
+if st.button('Predict'):
+    if input_sms:  # Check if input is not empty
+        # 1. Preprocess
+        transformed_sms = transform_text(input_sms)
+
+        # 2. Vectorize
+        vector_input = tfidf.transform([transformed_sms])
+
+        # 3. Predict
+        result = model.predict(vector_input)[0]
+
+        # 4. Display result
+        if result == 1:
+            st.header("Spam")
+        else:
+            st.header("Not Spam")
     else:
-        st.header("Not Spam")
+        st.warning("Please enter a message.")
