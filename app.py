802cf1500c0beec6c7d1b4d3812a1ef17f7bf83e
Your Name <chakrabortysuman599@gmail.com>
2025-01-22 23:15:38 +0530

    all good

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.stem.porter import PorterStemmer
 ps=PorterStemmer()
-nltk.download('punkt')
+
 
 
 def transform_text(text):
